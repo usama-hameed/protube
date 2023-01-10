815618321da89fb3cf29ce66d0c509b613ecd85f
usama-hameed <musamahameed97@gmail.com>
2023-01-11 02:10:30 +0500

    Models completed

--- main/models.py
+++ main/models.py
@@ -5,12 +5,10 @@ from datetime import date
 # Create your models here.
 
 
-class Comments(models.Model):
-    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment_date = models.DateField(default=date.today)
-    comment_text = models.TextField(max_length=200)
-    like = models.IntegerField(default=0)
-    dislike = models.IntegerField(default=0)
+class Playlist(models.Model):
+    name = models.CharField(max_length=225, default='', null=False, unique=True)
+    created_date = models.DateField(default=date.today)
+    # video = models.ForeignKey(Videos, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Videos(models.Model):
@@ -19,10 +17,14 @@ class Videos(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     dislike = models.IntegerField(default=0)
-    comment = models.ForeignKey(Comments, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, blank=True, null=True)
+    # comment = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
 
 
-class Playlist(models.Model):
-    name = models.CharField(max_length=225, default='', null=False, unique=True)
-    created_date = models.DateField(default=date.today)
-
+class Comments(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_date = models.DateField(default=date.today)
+    comment_text = models.TextField(max_length=200)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    video = models.ForeignKey(Videos, on_delete=models.CASCADE, blank=True, null=True)
